e9eb80871f68dd6368335dab250f4a19150fa119
DinaraDervel <dinaradervel@gmail.com>
2025-01-02 11:18:27 -0600

    add banner3 component

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,3 +1,4 @@
+import Banner3 from '@/components/Banner3/Banner3';
 import styles from './page.module.scss';
 import Header from '@/components/Header/Header';
 
@@ -5,6 +6,7 @@ export default async function Home() {
   return (
     <div className={styles.main}>
       <Header />
+      <Banner3 />
     </div>
   );
 }
