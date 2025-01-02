0383ab84a8f9288e2de0258be4781bbf2cbbe283
Daria Demidova <daashenkaa@mail.ru>
2025-01-02 16:32:54 +0300

    feat: add banner2

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,10 +1,12 @@
 import styles from './page.module.scss';
 import Header from '@/components/Header/Header';
+import Banner2 from '@/components/Banner2/Banner2';
 
 export default async function Home() {
   return (
     <div className={styles.main}>
       <Header />
+      <Banner2 />
     </div>
   );
 }
