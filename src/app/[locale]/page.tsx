18bda08f64bea381ba7bc58d6851a56da68089dc
Nadia-HPoe <isslandia@mail.ru>
2025-01-05 12:28:03 +0100

    create c-level component

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -2,6 +2,7 @@ import Banner1 from '@/components/Banner1/Banner1';
 import styles from './page.module.scss';
 import Header from '@/components/Header/Header';
 import Banner2 from '@/components/Banner2/Banner2';
+import CLevel from '@/components/C-Level/CLevel';
 
 export default async function Home() {
   return (
@@ -9,6 +10,8 @@ export default async function Home() {
       <Header />
       <Banner1 />
       <Banner2 />
+
+      <CLevel />
     </div>
   );
 }
