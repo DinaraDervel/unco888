6f63cd722299b0f1ab2b162c13812f4d279dcbe6
Alex <pr9nek@mail.ru>
2025-01-15 21:37:01 +0300

    fix: ContactComponent-tablet version

--- src/components/ModalСomponent/ModalСomponent.tsx
+++ src/components/ModalСomponent/ModalСomponent.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { ReactElement, useState, useEffect, useCallback } from "react";
+import React, { ReactElement, useState, useEffect } from "react";
 import Modal from "react-modal";
 import styles from "./ModalComponent.module.scss";
 interface ChildrenProps {
