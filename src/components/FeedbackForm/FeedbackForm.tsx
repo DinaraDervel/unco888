30026f4532710101a12595afc0c5f4c796a3bcb6
Eugenia <110728681+Emmnia@users.noreply.github.com>
2025-01-19 19:13:41 +0300

    feat: create feedback form

--- src/components/FeedbackForm/FeedbackForm.tsx
+++ src/components/FeedbackForm/FeedbackForm.tsx
@@ -1,24 +1,31 @@
 "use client";
-import {useTranslations} from "next-intl";
-import styles from "@/components/ContactForm/contactForm.module.scss";
 import React, {ChangeEvent, useState} from "react";
+import {useTranslations} from "next-intl";
+import ModalComponent from "@/components/ModalСomponent/ModalСomponent";
+import Button3 from "@/components/Button3/Button3";
+import styles from "@/components/FeedbackForm/FeedbackForm.module.scss";
 
 type FeedbackFormProps = {
     isOpen: boolean;
+    onClose: () => void;
 };
 
-const FeedbackForm: React.FC<FeedbackFormProps> = ({isOpen}) => {
+const FeedbackForm: React.FC<FeedbackFormProps> = ({isOpen, onClose}) => {
     const t = useTranslations('feedbackForm');
 
     const [name, setName] = useState<string>('');
     const [photo, setPhoto] = useState<File | null>(null);
     const [message, setMessage] = useState<string>('');
     const [isAgreed, setIsAgreed] = useState<boolean>(false);
+    const [touched, setTouched] = useState<boolean>(false);
 
     const closeModal = () => {
         setName('');
         setPhoto(null);
         setMessage('');
+        setIsAgreed(false);
+        setTouched(false);
+        onClose();
     };
 
     const handlePhotoChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -36,61 +43,80 @@ const FeedbackForm: React.FC<FeedbackFormProps> = ({isOpen}) => {
         closeModal();
     };
 
+    const isFormValid = name.trim() !== '' && message.trim() !== '' && isAgreed;
+
     return (
-            <form className={styles.main_container_contact_form}>
-                <div className={styles.wrapper_contact_form}>
-                    <button className={styles.close} onClick={closeModal} disabled={false}/>
+        <ModalComponent isOpen={isOpen} onRequestClose={closeModal}>
+            <form className={styles.feedback_form}>
+                <div className={styles.container_feedback_form}>
+                    <button type="button" className={styles.close} onClick={closeModal}/>
                     <div>
                         <div>
-                            <label htmlFor="name">{t("labelName")}</label>
+                            <label className={styles.label} htmlFor="name">{t("labelName")}</label>
                             <input
+                                className={`${styles.input} ${touched ? styles.error : ""}`}
                                 type="text"
                                 id="name"
                                 value={name}
                                 placeholder={t("placeholderName")}
                                 onChange={(e) => setName(e.target.value)}
+                                onBlur={() => setTouched(true)}
                                 required
                             />
+                        </div>
+                        <div className={`${styles.input} ${styles.photo}`}>
+                            <label className={styles.photo_text} htmlFor="photo">
+                                {t("placeholderPhoto")}
+                            </label>
                             <input
+                                className={styles.hidden}
                                 type="file"
                                 id="photo"
                                 accept="image/*"
-                                placeholder={t("placeholderPhoto")}
                                 onChange={handlePhotoChange}
                             />
-                        </div>
-                        <div className={styles.container_message}>
-                            <label htmlFor="message">{t("labelMessage")}</label>
-                            <textarea
-                                placeholder={t('placeholderMessage')}
-                                id="message"
-                                value={message}
-                                onChange={(e) => setMessage(e.target.value)}
-                                required
-                            />
+                            {photo && (
+                                <p className={styles.photo_text}>
+                                    {t("selectedPhoto")}: {photo.name}
+                                </p>
+                            )}
                         </div>
                     </div>
-                    <footer>
-                        <div className={styles.container_agree}>
-                            <input
-                                id="agree"
-                                className={styles.agree}
-                                checked={isAgreed}
-                                onChange={handleAgreementChange}
-                                type="checkbox"
-                                required
-                            />
-                            <label className={styles.text_label} htmlFor="agree">{t("agreeText")}</label>
-                        </div>
-                        <button
-                            type="submit"
-                            onClick={handleSubmit}
-                            disabled={!isAgreed}>
-                            {t('btnText')}
-                        </button>
-                    </footer>
+                    <div>
+                        <label className={styles.label} htmlFor="message">{t("labelMessage")}</label>
+                        <textarea
+                            className={`${styles.input} ${styles.message} ${touched ? styles.error : ""}`}
+                            placeholder={t('placeholderMessage')}
+                            id="message"
+                            value={message}
+                            onChange={(e) => setMessage(e.target.value)}
+                            onBlur={() => setTouched(true)}
+                            required
+                        />
+                    </div>
                 </div>
+                <footer className={styles.footer}>
+                    <label className={styles.container_agree} htmlFor="agree">
+                        <input
+                            id="agree"
+                            className={styles.checkbox_agree}
+                            checked={isAgreed}
+                            onChange={handleAgreementChange}
+                            type="checkbox"
+                            required
+                        />
+                        <span className={styles.text_agree}>{t("agreeText")}</span>
+                    </label>
+                    <Button3
+                        className={`${styles.button} ${styles.submit}`}
+                        type="submit"
+                        onClick={handleSubmit}
+                        disabled={!isFormValid}
+                        text={t("btnText")}
+                    />
+                </footer>
             </form>
+        </ModalComponent>
     )
 }
 
