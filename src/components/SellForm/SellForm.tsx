6fefac7b029e7a7615e0d309107fdc604b60f1df
Eugenia <110728681+Emmnia@users.noreply.github.com>
2025-02-01 15:56:32 +0300

    fix: make submit btn disabled - no price input yet

--- src/components/SellForm/SellForm.tsx
+++ src/components/SellForm/SellForm.tsx
@@ -1,10 +1,10 @@
 'use client';
 
-import { ChangeEvent, useState } from 'react';
-import Button from '../Button/Button';
+import React, { ChangeEvent, useState } from 'react';
 import styles from './sellForm.module.scss';
 import { useTranslations } from 'next-intl';
 import Link from 'next/link';
+import Button3 from '@/components/Button3/Button3';
 
 type SellFormProps = {
   onClose: () => void;
@@ -13,15 +13,48 @@ type SellFormProps = {
 const SellForm: React.FC<SellFormProps> = ({ onClose }) => {
   const t = useTranslations('sellForm');
   const [isAgreed, setIsAgreed] = useState<boolean>(false);
+  const [quantity, setQuantity] = useState<string>('');
+  const [price, setPrice] = useState<string>('');
+  const [link, setLink] = useState<string>('');
+  const [contact, setContact] = useState<string>('');
+  const [touchedFields, setTouchedFields] = useState({
+    quantity: false,
+    price: false,
+    link: false,
+    contact: false,
+  });
 
-  const handleCloseClick = (e: React.MouseEvent) => {
-    e.preventDefault();
+  const handleCloseClick = () => {
+    setQuantity('');
+    setPrice('');
+    setLink('');
+    setContact('');
+    setTouchedFields({
+      quantity: false,
+      price: false,
+      link: false,
+      contact: false,
+    });
     onClose();
+  };
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    handleCloseClick();
   };
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     setIsAgreed(event.target.checked);
   };
+
+  const handleBlur = (fieldName: 'quantity' | 'price' | 'link' | 'contact') => {
+    setTouchedFields((prev) => ({
+      ...prev,
+      [fieldName]: true,
+    }));
+  };
+
+  const isFormValid = quantity.trim() !== '' && price.trim() !== '' && link.trim() !== '' && contact.trim() !== '' && isAgreed;
 
   return (
     <form className={styles.container}>
@@ -31,7 +64,12 @@ const SellForm: React.FC<SellFormProps> = ({ onClose }) => {
         <input
           placeholder={t('placeholderQuantity')}
           type='number'
-          className={styles.container__input}
+          id='quantity'
+          value={quantity}
+          onChange={(e) => setQuantity(e.target.value)}
+          onBlur={() => handleBlur('quantity')}
+          className={`${styles.container__input} ${touchedFields.quantity && quantity.trim() === '' ? styles.error : ''}`}
+          required
         />
       </div>
 
@@ -43,7 +81,16 @@ const SellForm: React.FC<SellFormProps> = ({ onClose }) => {
             opensea.io
           </Link>
         </p>
-        <input placeholder={t('placeholderLink')} type='url' className={styles.container__input} />
+        <input
+          placeholder={t('placeholderLink')}
+          type='url'
+          id='link'
+          value={link}
+          onChange={(e) => setLink(e.target.value)}
+          onBlur={() => handleBlur('link')}
+          className={`${styles.container__input} ${touchedFields.link && link.trim() === '' ? styles.error : ''}`}
+          required
+        />
       </div>
 
       <div className={styles.container__wrapper}>
@@ -51,7 +98,12 @@ const SellForm: React.FC<SellFormProps> = ({ onClose }) => {
         <input
           placeholder={t('placeholderContactInfo')}
           type='text'
-          className={styles.container__input}
+          id='contact'
+          value={contact}
+          onChange={(e) => setContact(e.target.value)}
+          onBlur={() => handleBlur('contact')}
+          className={`${styles.container__input} ${touchedFields.contact && contact.trim() === '' ? styles.error : ''}`}
+          required
         />
       </div>
       <div className={styles.container__bottom}>
@@ -62,13 +114,20 @@ const SellForm: React.FC<SellFormProps> = ({ onClose }) => {
             checked={isAgreed}
             onChange={handleChange}
             type='checkbox'
+            required
           />
           <label className={styles.text_label} htmlFor='agree'>
             {t('agreeText')}
           </label>
         </div>
         <div className={styles.buttonContainer}>
-          <Button text={t('btnText')} link='' className={styles.button} />
+          <Button3
+            className={`${styles.button} ${styles.submit}`}
+            type='submit'
+            onClick={handleSubmit}
+            disabled={!isFormValid}
+            text={t('btnText')}
+          />
         </div>
       </div>
     </form>
