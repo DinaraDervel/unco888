b2b0c46a150f7a8ed125b78adc869eb6e8f795a1
DinaraDervel <dinaradervel@gmail.com>
2025-02-12 18:50:19 -0600

    feat: add loading data for BuyandSell and sending SellForm to google sheets

---
+++ src/app/[locale]/actions.ts
@@ -0,0 +1,54 @@
+'use server';
+
+import { initGoogleAPI } from '@/server-actions/google-sheets';
+
+const GOOGLE_SHEET_SELLOFFERS_RANGE = process.env.GOOGLE_SHEET_SELLOFFERS_RANGE;
+
+export const loadSellOffers = async () => {
+  const { sheets, spreadsheetId, range } = await initGoogleAPI(GOOGLE_SHEET_SELLOFFERS_RANGE);
+
+  if (sheets) {
+    try {
+      const getRows = await sheets.spreadsheets.values.get({
+        spreadsheetId,
+        range,
+      });
+      console.log(getRows);
+      return { data: getRows.data.values as string[][], status: getRows.status };
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  return { data: null, status: 500 };
+};
+
+export const sendSellOffer = async (
+  quantity: string,
+  price: string,
+  link: string,
+  contact: string,
+  visible: boolean
+) => {
+  const { sheets, spreadsheetId } = await initGoogleAPI();
+
+  try {
+    if (!sheets) throw new Error('Server error');
+    const sheetsRes = await sheets.spreadsheets.values.append({
+      spreadsheetId,
+      range: GOOGLE_SHEET_SELLOFFERS_RANGE,
+      valueInputOption: 'USER_ENTERED',
+      requestBody: {
+        values: [[Date.now(), quantity, price, link, contact, visible]],
+      },
+    });
+    return { status: sheetsRes.status, error: '' };
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error(error);
+      return { status: 500, error: error.message };
+    } else {
+      console.log('Unknown error');
+    }
+  }
+  return { status: 500, error: '' };
+};
